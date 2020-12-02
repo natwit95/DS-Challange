1f07c31cb105e286f4af0ba864a1314ae597df92
Natalia Wit <natwit95@gmail.com>
2020-12-02 16:20:09 -0500

    set url path to variable

--- hcsc.js
+++ hcsc.js
@@ -2,9 +2,10 @@
 //map through client enviroments to get back an array of dates
 //platform-api has "-" symbol and needs to be placed in square bracket since dot notation doesn suppoer "-"[]
 
-fetch(
-	"https://gist.githubusercontent.com/gretchenziegler/853c4f709d45176aa44c8e5aee864cac/raw/010a4c44455ffc93b8039935cfc4e0dff41ae502/hcsc.json"
-)
+const clientInfoUrl =
+	"https://gist.githubusercontent.com/gretchenziegler/853c4f709d45176aa44c8e5aee864cac/raw/010a4c44455ffc93b8039935cfc4e0dff41ae502/hcsc.json";
+
+fetch(clientInfoUrl)
 	.then((resp) => resp.json())
 	.then((data) =>
 		console.log(
